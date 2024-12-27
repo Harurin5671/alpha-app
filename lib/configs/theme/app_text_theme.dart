e3206589b6fce25535edccfe9750775ec8a5a416
Frank Santos <frank.santosg.dev@gmail.com>
2024-12-27 16:36:41 -0500

    -Ajustando lógica para el calculo de la escala en los textos dependiendo de la plataforma -Ajustes en la forma de usar el AppTheme para enviar el context y facilitar el calculo de la escala

--- lib/configs/theme/app_text_theme.dart
+++ lib/configs/theme/app_text_theme.dart
@@ -1,160 +1,178 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTextTheme {
-  static TextTheme lightTextTheme = TextTheme(
-    bodyLarge: GoogleFonts.nunito(
-      fontSize: 14,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    bodyMedium: GoogleFonts.nunito(
-      fontSize: 12,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    bodySmall: GoogleFonts.nunito(
-      fontSize: 10,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    headlineLarge: GoogleFonts.nunito(
-      fontSize: 22,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    headlineMedium: GoogleFonts.nunito(
-      fontSize: 18,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    headlineSmall: GoogleFonts.nunito(
-      fontSize: 16,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    labelLarge: GoogleFonts.nunito(
-      fontSize: 16,
-      height: 1.5,
-      color: Color(0xffB2BCC9),
-    ),
-    labelMedium: GoogleFonts.nunito(
-      fontSize: 14,
-      height: 1.5,
-      color: Color(0xffB2BCC9),
-    ),
-    labelSmall: GoogleFonts.nunito(
-      fontSize: 12,
-      height: 1.5,
-      color: Color(0xffB2BCC9),
-    ),
-    titleLarge: GoogleFonts.nunito(
-      fontSize: 22,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    titleMedium: GoogleFonts.nunito(
-      fontSize: 18,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    titleSmall: GoogleFonts.nunito(
-      fontSize: 16,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    displayLarge: GoogleFonts.nunito(
-      fontSize: 40,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    displayMedium: GoogleFonts.nunito(
-      fontSize: 34,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-    displaySmall: GoogleFonts.nunito(
-      fontSize: 28,
-      height: 1.5,
-      color: Color(0xff33384B),
-    ),
-  );
-  static TextTheme darkTextTheme = TextTheme(
-    bodyLarge: GoogleFonts.nunito(
-      fontSize: 14,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    bodyMedium: GoogleFonts.nunito(
-      fontSize: 12,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    bodySmall: GoogleFonts.nunito(
-      fontSize: 10,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    headlineLarge: GoogleFonts.nunito(
-      fontSize: 22,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    headlineMedium: GoogleFonts.nunito(
-      fontSize: 18,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    headlineSmall: GoogleFonts.nunito(
-      fontSize: 16,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    labelLarge: GoogleFonts.nunito(
-      fontSize: 16,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    labelMedium: GoogleFonts.nunito(
-      fontSize: 14,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    labelSmall: GoogleFonts.nunito(
-      fontSize: 12,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    titleLarge: GoogleFonts.nunito(
-      fontSize: 22,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    titleMedium: GoogleFonts.nunito(
-      fontSize: 18,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    titleSmall: GoogleFonts.nunito(
-      fontSize: 16,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    displayLarge: GoogleFonts.nunito(
-      fontSize: 40,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    displayMedium: GoogleFonts.nunito(
-      fontSize: 34,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-    displaySmall: GoogleFonts.nunito(
-      fontSize: 28,
-      height: 1.5,
-      color: Color(0xffF1F4F6),
-    ),
-  );
+  static double _getScaleFactor(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth > 1200) {
+      return 1.3;
+    } else if (screenWidth > 800) {
+      return 1.2;
+    }
+    return 1.0;
+  }
+
+  static TextTheme buildResponsiveTextTheme(BuildContext context) {
+    double scaleFactor = _getScaleFactor(context);
+
+    return TextTheme(
+      bodyLarge: GoogleFonts.nunito(
+        fontSize: 14 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      bodyMedium: GoogleFonts.nunito(
+        fontSize: 12 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      bodySmall: GoogleFonts.nunito(
+        fontSize: 10 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      headlineLarge: GoogleFonts.nunito(
+        fontSize: 22 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      headlineMedium: GoogleFonts.nunito(
+        fontSize: 18 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      headlineSmall: GoogleFonts.nunito(
+        fontSize: 16 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      labelLarge: GoogleFonts.nunito(
+        fontSize: 16 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffB2BCC9),
+      ),
+      labelMedium: GoogleFonts.nunito(
+        fontSize: 14 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffB2BCC9),
+      ),
+      labelSmall: GoogleFonts.nunito(
+        fontSize: 12 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffB2BCC9),
+      ),
+      titleLarge: GoogleFonts.nunito(
+        fontSize: 22 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      titleMedium: GoogleFonts.nunito(
+        fontSize: 18 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      titleSmall: GoogleFonts.nunito(
+        fontSize: 16 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      displayLarge: GoogleFonts.nunito(
+        fontSize: 40 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      displayMedium: GoogleFonts.nunito(
+        fontSize: 34 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+      displaySmall: GoogleFonts.nunito(
+        fontSize: 28 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xff33384B),
+      ),
+    );
+  }
+
+  static TextTheme buildResponsiveDarkTextTheme(BuildContext context) {
+    double scaleFactor = _getScaleFactor(context);
+
+    return TextTheme(
+      bodyLarge: GoogleFonts.nunito(
+        fontSize: 14 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      bodyMedium: GoogleFonts.nunito(
+        fontSize: 12 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      bodySmall: GoogleFonts.nunito(
+        fontSize: 10 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      headlineLarge: GoogleFonts.nunito(
+        fontSize: 22 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      headlineMedium: GoogleFonts.nunito(
+        fontSize: 18 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      headlineSmall: GoogleFonts.nunito(
+        fontSize: 16 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      labelLarge: GoogleFonts.nunito(
+        fontSize: 16 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      labelMedium: GoogleFonts.nunito(
+        fontSize: 14 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      labelSmall: GoogleFonts.nunito(
+        fontSize: 12 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      titleLarge: GoogleFonts.nunito(
+        fontSize: 22 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      titleMedium: GoogleFonts.nunito(
+        fontSize: 18 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      titleSmall: GoogleFonts.nunito(
+        fontSize: 16 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      displayLarge: GoogleFonts.nunito(
+        fontSize: 40 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      displayMedium: GoogleFonts.nunito(
+        fontSize: 34 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+      displaySmall: GoogleFonts.nunito(
+        fontSize: 28 * scaleFactor,
+        height: 1.5,
+        color: const Color(0xffF1F4F6),
+      ),
+    );
+  }
 }
--- lib/configs/theme/app_theme.dart
+++ lib/configs/theme/app_theme.dart
@@ -3,108 +3,165 @@ import 'package:flutter/material.dart';
 import 'app_text_theme.dart';
 
 class AppTheme {
-  static final lightTheme = ThemeData(
-    useMaterial3: true,
-    textTheme: AppTextTheme.lightTextTheme,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0xffffffff),
-      centerTitle: true,
-      elevation: 0,
-      scrolledUnderElevation: 0,
-    ),
-    brightness: Brightness.light,
-    scaffoldBackgroundColor: Color(0xffffffff),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Color(0xffF4F4F6),
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(16),
+  static ThemeData buildLightTheme(BuildContext context) {
+    return ThemeData(
+      useMaterial3: true,
+      textTheme: AppTextTheme.buildResponsiveTextTheme(context),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xffffffff),
+        centerTitle: true,
+        elevation: 0,
+        scrolledUnderElevation: 0,
       ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Color(0xffF4F4F6),
-          width: 1,
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: Color(0xffffffff),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xffF4F4F6),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(16),
         ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      errorBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Color(0xffF50036),
-          width: 1,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xffF4F4F6),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(16),
         ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      errorStyle: AppTextTheme.lightTextTheme.labelMedium?.copyWith(
-        color: Color(0xffF50036),
-      ),
-      filled: true,
-      fillColor: Color(0xffFAFAFC),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Color(0xff2E6FF3),
-          width: 1,
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xffF50036),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(16),
         ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      floatingLabelBehavior: FloatingLabelBehavior.always,
-      hintStyle: AppTextTheme.lightTextTheme.labelMedium
-          ?.copyWith(color: Color(0xffB2BCC9)),
-      labelStyle: AppTextTheme.lightTextTheme.labelLarge
-          ?.copyWith(color: Color(0xff33384B)),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: WidgetStatePropertyAll<Color>(
-          Color(0xff2E6FF3),
+        errorStyle: AppTextTheme.buildResponsiveTextTheme(context)
+            .labelMedium
+            ?.copyWith(
+              color: Color(0xffF50036),
+            ),
+        filled: true,
+        fillColor: Color(0xffFAFAFC),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xff2E6FF3),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(16),
         ),
-        minimumSize: WidgetStatePropertyAll<Size>(
-          Size(double.infinity, 56),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        hintStyle: AppTextTheme.buildResponsiveTextTheme(context)
+            .labelMedium
+            ?.copyWith(color: Color(0xffB2BCC9)),
+        labelStyle: AppTextTheme.buildResponsiveTextTheme(context)
+            .labelLarge
+            ?.copyWith(color: Color(0xff33384B)),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll<Color>(
+            Color(0xff2E6FF3),
+          ),
+          minimumSize: WidgetStatePropertyAll<Size>(
+            Size(double.infinity, 56),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 
-  static final darkTheme = ThemeData(
-    appBarTheme: AppBarTheme(
-      backgroundColor: Color(0xff232528),
-    ),
-    brightness: Brightness.dark,
-    useMaterial3: true,
-    textTheme: AppTextTheme.darkTextTheme,
-    scaffoldBackgroundColor: Color(0xff232528),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(
-        borderSide: BorderSide.none,
-        borderRadius: BorderRadius.circular(16),
+  static ThemeData buildDarkTheme(BuildContext context) {
+    return ThemeData(
+      appBarTheme: AppBarTheme(
+        backgroundColor: Color(0xff232528),
+        elevation: 0,
+        scrolledUnderElevation: 0,
       ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide.none,
+      brightness: Brightness.dark,
+      useMaterial3: true,
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: Color(0xff2E6FF3),
+        selectionColor: Color(0xff2E6FF3),
+        selectionHandleColor: Color(0xff7D8A95),
       ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide(
-          color: Color(0xff2E6FF3),
-          width: 1,
+      textTheme: AppTextTheme.buildResponsiveDarkTextTheme(context),
+      scaffoldBackgroundColor: Color(0xff232528),
+      inputDecorationTheme: InputDecorationTheme(
+        errorMaxLines: 2,
+        errorStyle: AppTextTheme.buildResponsiveDarkTextTheme(context)
+            .bodySmall
+            ?.copyWith(
+              color: Color(0xffE86666),
+            ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Color(0xffE86666),
+            width: 1,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Color(0xffE86666),
+            width: 1,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Color(0xff2E6FF3),
+            width: 1,
+          ),
+        ),
+        filled: true,
+        fillColor: Color(0xff2B3037),
+        hintStyle: AppTextTheme.buildResponsiveDarkTextTheme(context)
+            .labelMedium
+            ?.copyWith(
+              color: Color(0xff7D8A95),
+            ),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        floatingLabelStyle: AppTextTheme.buildResponsiveDarkTextTheme(context)
+            .labelMedium
+            ?.copyWith(
+              color: Color(0xffF1F4F6),
+            ),
+        suffixIconColor: WidgetStateColor.resolveWith((states) {
+          if (states.contains(WidgetState.error)) {
+            return Color(0xffE86666);
+          }
+          return Color(0xff7D8A95);
+        }),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.resolveWith(
+            (states) {
+              if (states.contains(WidgetState.disabled)) {
+                return null;
+              }
+              return const Color(0xff2E6FF3);
+            },
+          ),
+          minimumSize: WidgetStatePropertyAll<Size>(
+            Size(
+              double.infinity,
+              56,
+            ),
+          ),
         ),
       ),
-      filled: true,
-      fillColor: Color(0xff2B3037),
-      hintStyle: AppTextTheme.darkTextTheme.labelMedium?.copyWith(
-        color: Color(0xff7D8A95),
-      ),
-      floatingLabelBehavior: FloatingLabelBehavior.always,
-      floatingLabelStyle: AppTextTheme.darkTextTheme.labelMedium?.copyWith(
-        color: Color(0xffF1F4F6),
-      ),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: WidgetStatePropertyAll<Color>(Color(0xff2E6FF3)),
-        minimumSize: WidgetStatePropertyAll<Size>(Size(double.infinity, 56)),
-      ),
-    ),
-  );
+    );
+  }
 }
